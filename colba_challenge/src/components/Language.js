81386f520945a41751f242cc17c41228d0bb5060
jc4rubio <juancarlosrubiogarci89@gmail.com>
2018-09-06 14:53:17 +0200

    Progress on Language section

--- colba_challenge/src/components/Language.js
+++ colba_challenge/src/components/Language.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import './Language.css';
 
-//import { languages } from '../mock_data/profile.json';
 
 class Language extends Component {
 
@@ -13,13 +12,49 @@ class Language extends Component {
         this.level = props.level;
     }
 
+    // This doesn't work! It's rendered before render and classes are not been applied: why??
     renderLevel = () => {
-        
+
+        //console.log('Level: '+this.level);
+        var class_names=["levelWeak", "levelWeak", "levelWeak"];
+
+        if (this.level === 1 || this.level === "1") { 
+            class_names = ["levelStrong", "levelWeak", "levelWeak"]; // Level 1
+        } else if (this.level === 2 || this.level === "2") {             
+            class_names = ["levelStrong", "levelStrong", "levelWeak"]; // Level 2
+        } else if (this.level === 3 || this.level === "3") {  
+            class_names = ["levelStrong", "levelStrong", "levelStrong"]; // Level 3
+        }
+
         return (
             <div>
-                <span className="rounded bg-success">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;&nbsp;
+                {
+                    class_names.map((class_name, i) => {
+
+                        return (
+                            <div key={'my_key'+i} className={"col-3 box rounded level "+class_name}></div>
+                        );
+                    })
+                }
             </div>
         );
+    }
+
+    // This do works!
+    getClassName = (index) => {
+
+        //console.log('Level: '+this.level);
+        var class_names=["levelWeak", "levelWeak", "levelWeak"];
+
+        if (this.level === 1 || this.level === "1") { 
+            class_names = ["levelStrong", "levelWeak", "levelWeak"]; // Level 1
+        } else if (this.level === 2 || this.level === "2") {             
+            class_names = ["levelStrong", "levelStrong", "levelWeak"]; // Level 2
+        } else if (this.level === 3 || this.level === "3") {  
+            class_names = ["levelStrong", "levelStrong", "levelStrong"]; // Level 3
+        }
+
+        return (class_names[index]);
     }
 
     render () {
@@ -31,9 +66,11 @@ class Language extends Component {
                 <div className="col-12 col-align-self-start">
                     {/*TODO: Iterate depending on level*/}
                     <div className="row my-2 justify-content-around">
-                        <div className="col-3 box rounded level"></div>
-                        <div className="col-3 box rounded level"></div>
-                        <div className="col-3 box rounded level"></div>
+
+                        <div className={"col-3 box rounded level "+ this.getClassName(0)}></div>
+                        <div className={"col-3 box rounded level "+ this.getClassName(1)}></div>
+                        <div className={"col-3 box rounded level "+ this.getClassName(2)}></div>
+                        {/* {this.renderLevel()} */} {/*TODO: To understand why this doesn't work fine*/}
                     </div>
                 </div>
                 <div className="col-12 my-3">
--- colba_challenge/src/components/Languages.js
+++ colba_challenge/src/components/Languages.js
@@ -18,10 +18,9 @@ class Languages extends Component {
         return (
             <div className="container mt-1 pb-2">
                 <div className="h5 sectionTitle">Languages</div>
-                <div className="row justify-content-start"> {/* TODO: Do it dinamically | Add level bars and description*/}
-                    <div className="col-4">{this.languages[0].language}</div>
-                    <div className="col-4">{this.languages[1].language}</div>
+                <div className="row justify-content-start"> {/* TODO: Add languages dinamically*/}
                     <div className="col-4"><Language language={this.languages[0].language} level={this.languages[0].level} description={this.languages[0].description}/></div>
+                    <div className="col-4"><Language language={this.languages[1].language} level={this.languages[1].level} description={this.languages[1].description}/></div>
                 </div>
             </div>
         );
