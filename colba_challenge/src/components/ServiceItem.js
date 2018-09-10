5734ba20b806f6f2f9288b40b23d173bc6efe9ff
jc4rubio <juancarlosrubiogarci89@gmail.com>
2018-09-10 11:48:28 +0200

    ServiceItem Enhancement: Toggle content

--- colba_challenge/src/components/ServiceItem.js
+++ colba_challenge/src/components/ServiceItem.js
@@ -2,62 +2,85 @@ import React, { Component } from 'react';
 
 import './ServiceItem.js';
 
-class ServiceItem extends Component {
-    
-    /*constructor(serviceItem) {
-        super();
-        this.category = serviceItem.category;
-        /*this.services = {
-            name,
-            fee,
-            currency,
-            rate
-        };
-        this.status;
-    }*/
+class ContentLite extends Component {
 
+    render() {
+        return (
+            <div className="row border-bottom border-1 py-2">
+                <div className="col-4">{"(-) "+this.props.service.rate} Stars</div> {/* TODO: Stars*/}
+                <div className="col-8 right"><span className="">{this.props.service.fee + " " + this.props.service.currency + " per hour"}</span></div> 
+            </div>
+        );
+    }
+}
+
+class ContentComplete extends Component {
+
+    render() {
+        return(
+            <div className="row py-3 mb-1" style={{backgroundColor: 'rgb(250, 250, 250'}}> 
+                <div className="col-6">Fee</div>
+                <div className="col-6 text-right"><span className="font-weight-bold">{this.props.service.fee + " " + this.props.service.currency + " per hour"}</span></div>
+            </div>
+        );
+    }
+}
+
+export default class ServiceItem extends Component {
+    
     constructor() {
         super();
-        this.services = {
+       
+        // Mock data
+        this.service = {
             name : 'Servicio 1',
             fee : 35,
             currency: "CHF",
             rate: 5
         }
-        this.status = 'Hide';
+
+        this.state = {
+            isShown: false,
+        };
+        
+        this.operation_text = "Show";
+        
+        this.src_img_down = "/images/arrow1-down.png";
+        this.src_img_up = "/images/arrow1-up.png";
+        this.src_img = this.src_img_down;
+
     }
 
-    /* TODO: Add functionality to span to toggle divs. That is, change span name; change icon; and toggle between div1 and div2*/
+    toggleContent = () => {
+
+        if(this.state.isShown) { 
+            // Hide
+            this.operation_text = "Show";
+            this.src_img = this.src_img_down;
+            
+        } else { 
+            // Shown
+            this.operation_text = "Hide";
+            this.src_img = this.src_img_up;
+        }
+        this.setState({ isShown: !this.state.isShown });
+    }
 
     render() {
 
         return (
-            <div className="container">
+            <div className="container ">
                 <div className="row">
-                    <div className="col-6">{this.services.name}</div>
-                    {/* <div className="col-6 text-right"><span id={this.services.name} className="">Show</span></div> */}
+                    <div className="col-6">{this.service.name}</div>
                     <div className="col-6 text-right">
-                        <span className="font-weight-bold text-success" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                            Show <img className="img-fluid" src="/images/arrow1-down.png" alt=""></img> {/*State 1: hidden (default) */}
-                            Hide <img className="img-fluid" src="/images/arrow1-up.png" alt=""></img> {/*State 1: shown => TODO: Remove it*/}
+                        <span className="font-weight-bold text-success" onClick={this.toggleContent}>
+                            {this.operation_text} 
+                            <img className="img-fluid ml-1" src={this.src_img} alt=""></img>
                         </span>
                     </div>
                 </div>
-                {/* Div 1: Shown by default */}
-                <div className="row visible">
-                    <div className="col-4">{"(-) "+this.services.rate} Stars</div> {/* TODO: Stars*/}
-                    <div className="col-8 right"><span className="feeShown">{this.services.fee + " " + this.services.currency + " per hour"}</span></div> 
-                </div>
-                {/* Div 2: Hidden by default */}
-                <div className="row invisible">
-                    <div className="col-6">Fee</div>
-                    <div className="col-6 text-right"><span className="feeHidden">{this.services.fee + " " + this.services.currency + " per hour"}</span></div> 
-                </div>
-            </div>
-
-            
+                {this.state.isShown? <ContentComplete service={this.service}/> : <ContentLite service={this.service}/>}
+            </div>   
         );
     }
 }
-
-export default ServiceItem;
