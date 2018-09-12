1c059aa43864be6138d5e34b1f479996cae3ae9a
jc4rubio <juancarlosrubiogarci89@gmail.com>
2018-09-12 13:38:32 +0200

    Updating ServiceItem, deleting mockData. Now data comes from parent

--- colba_challenge/src/components/ServiceItem.js
+++ colba_challenge/src/components/ServiceItem.js
@@ -28,27 +28,31 @@ class ContentComplete extends Component {
 
 export default class ServiceItem extends Component {
     
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
        
-        // Mock data
-        this.service = {
-            name : 'Servicio 1',
-            fee : 35,
-            currency: "CHF",
-            rate: 5
-        }
-
+        // Doubt: When to use a property inside or outside of state: src_img / operation_text  
+        this.operation_text = "Show"; // Default value (Show/Hide)
+        this.src_img_down = "/images/arrow1-down.png"; // This can be rotated 180 degrees with CSS to avoid having two images
+        this.src_img_up = "/images/arrow1-up.png";
+        
         this.state = {
             isShown: false,
+            src_img: this.src_img_down
         };
-        
-        this.operation_text = "Show";
-        
-        this.src_img_down = "/images/arrow1-down.png";
-        this.src_img_up = "/images/arrow1-up.png";
-        this.src_img = this.src_img_down;
 
+        // Default Service /* TODO: How to handle when there is no arguments/props?  */
+        const default_service = {
+            name : 'Default Service',
+            fee : 5,
+            currency: "€",
+            rate: 1  
+        }
+        if(!props.service) {
+            this.service = default_service;
+        } else {
+            this.service = props.service;
+        }
     }
 
     toggleContent = () => {
@@ -56,26 +60,28 @@ export default class ServiceItem extends Component {
         if(this.state.isShown) { 
             // Hide
             this.operation_text = "Show";
-            this.src_img = this.src_img_down;
+            this.setState({ src_img: this.src_img_down});
+            //this.src_img = this.src_img_down;
             
         } else { 
             // Shown
             this.operation_text = "Hide";
-            this.src_img = this.src_img_up;
+            this.setState({ src_img: this.src_img_up});
+            //this.src_img = this.src_img_up;
         }
         this.setState({ isShown: !this.state.isShown });
     }
 
     render() {
-
         return (
             <div className="container ">
                 <div className="row">
-                    <div className="col-6">{this.service.name}</div>
-                    <div className="col-6 text-right">
-                        <span className="font-weight-bold text-success" onClick={this.toggleContent}>
+                
+                    <div className="col-9 text-truncate px-0">{this.service.name}</div>
+                    <div className="col-3 text-right px-0">
+                        <span className="font-weight-bold text-success text-nowrap" onClick={this.toggleContent}>
                             {this.operation_text} 
-                            <img className="img-fluid ml-1" src={this.src_img} alt=""></img>
+                            <img className="img-fluid ml-1" src={this.state.src_img} alt=""></img>
                         </span>
                     </div>
                 </div>
